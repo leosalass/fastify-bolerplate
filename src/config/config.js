82682f0f57eb191713ed242d528e34afbf6f876f
Leonardo Salas <leosalass@gmail.com>
2023-02-13 22:29:47 -0500

    moved fastifyPrintRoutes

--- src/config/config.js
+++ src/config/config.js
@@ -6,12 +6,14 @@ import * as Mongoose from '../config/mongoose-config.js';
 import userRoutes from  '../routes/users.js';
 import * as Favicon from './favicon-config.js';
 import fastifyMultipart from '@fastify/multipart'
+import fastifyPrintRoutes from 'fastify-print-routes'
 
 export async function set(app, env) {
   Helper.set(app, env);
   Cors.set(app);
   Mongoose.set(app);
   app.register(fastifyMultipart);
+  app.register(fastifyPrintRoutes)
   app.register(userRoutes, { prefix: '/api/v1' });
   Favicon.set(app);
 }
